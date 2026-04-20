855d2346107b8ebdc5a411ed0fefdaf4360867ae
Dekfor <dekfor@yandex.ru>
2026-04-21 01:00:10 +0300

    Вынес url в data/

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium import webdriver
+from data.urls import BASE_URL
 
 
 @pytest.fixture
 def driver():
     driver = webdriver.Firefox()
-    driver.get("https://qa-scooter.praktikum-services.ru/")
+    driver.get(BASE_URL)
 
     yield driver
 
