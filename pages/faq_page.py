6810495b6daafff70990eb458d5cf3d31071a120
Dekfor <dekfor@yandex.ru>
2026-04-20 23:46:06 +0300

    Добавил allure.step над каждым методом page/

--- pages/faq_page.py
+++ pages/faq_page.py
@@ -1,11 +1,12 @@
 from pages.base_page import BasePage
 from locators.faq_locators import FaqLocators
 from selenium.webdriver.support import expected_conditions as EC
-import time
+import time, allure
 
 
 class FaqPage(BasePage):
 
+    @allure.step("Открыть вопрос FAQ")
     def open_question(self, index):
         question = self.wait_clickable(FaqLocators.QUESTIONS[index])
         self.scroll_to(question)
@@ -18,6 +19,7 @@ class FaqPage(BasePage):
         self.wait.until(
             EC.visibility_of_element_located(FaqLocators.ANSWERS[index])
         )
-
+        
+    @allure.step("Получить ответ FAQ")
     def get_answer(self, index):
         return self.get_text(FaqLocators.ANSWERS[index])
--- pages/order_page.py
+++ pages/order_page.py
@@ -1,10 +1,11 @@
 from pages.base_page import BasePage
 from locators.order_page_locators import OrderPageLocators
 from selenium.webdriver.common.by import By
-
+import allure
 
 class OrderPage(BasePage):
 
+    @allure.step("Заполнить персональные данные")
     def fill_personal_info(self, name, lastname, address, phone):
         self.type(OrderPageLocators.NAME, name)
         self.type(OrderPageLocators.LASTNAME, lastname)
@@ -13,6 +14,7 @@ class OrderPage(BasePage):
         self.select_metro()
         self.type(OrderPageLocators.PHONE, phone)
 
+    @allure.step("Выбрать метро")
     def select_metro(self):
         self.click(OrderPageLocators.METRO)
 
@@ -21,13 +23,16 @@ class OrderPage(BasePage):
         options = self.find_elements(first_option)
         options[0].click()
 
+    @allure.step("Нажать Далее")
     def click_next(self):
         self.click(OrderPageLocators.NEXT_BUTTON)
 
+    @allure.step("Выбрать дату доставки")
     def select_date(self):
         self.type(OrderPageLocators.DATE_INPUT, "31.01.2999")
         self.click((By.CSS_SELECTOR, ".App_App__15LM-"))
 
+    @allure.step("Выбрать срок аренды")
     def select_rent_period(self):
         self.click(OrderPageLocators.RENT_DROPDOWN)
         option = (By.CSS_SELECTOR, ".Dropdown-option")
@@ -37,24 +42,31 @@ class OrderPage(BasePage):
         options[0].click()
         self.click((By.CSS_SELECTOR, ".App_App__15LM-"))
 
+    @allure.step("Выбрать цвет")
     def select_color(self):
         option = (By.CSS_SELECTOR, "label.Checkbox_Label__3wxSf")
         self.click(option)
 
+    @allure.step("Нажать заказать")
     def click_order(self):
         self.click(OrderPageLocators.ORDER_BUTTON)
 
+    @allure.step("Подтвердить заказ")
     def confirm_order(self):
         self.click(OrderPageLocators.CONFIRM_BUTTON)
 
+    @allure.step("Получить текст подтверждения")
     def get_confirm_text(self):
         return self.get_text(OrderPageLocators.CONFIRM_TEXT)
     
+    @allure.step("Ожидание модалки успешности заказа")
     def wait_success_modal(self):
         self.wait_visible(OrderPageLocators.SUCCESS_HEADER)
         
+    @allure.step("Получить текст успешности заказа")
     def get_success_text(self):
         return self.get_text(OrderPageLocators.SUCCESS_HEADER)
     
+    @allure.step("Обновить страницу")
     def refresh_page(self):
         self.refresh_page()
